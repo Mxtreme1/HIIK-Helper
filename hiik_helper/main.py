97c063e1a0feb46924dbef02a1ea470b34758783
Maximilian <mr492@students.uni-freiburg.de>
2024-08-25 13:31:55 +0200

    HIIKDefaultSpider now working, extracting articles, but has to be restarted

--- hiik_helper/main.py
+++ hiik_helper/main.py
@@ -1,6 +1,14 @@
 from scrapy.crawler import CrawlerProcess, CrawlerRunner
 from scrapy.utils.project import get_project_settings
 from spiders.hiik_default_spider import HIIKDefaultSpider
+import logging
+# Set up logger and logger format
+logging.basicConfig(level=logging.INFO)
+formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+logger = logging.getLogger(__name__)
+logger.info("Logger set up")
 
 
 def main():
@@ -13,7 +21,7 @@ def main():
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
     )
 
-    runner = CrawlerProcess(settings=settings)
+    runner = CrawlerProcess(settings=settings, install_root_handler=False)
     runner.crawl(
         HIIKDefaultSpider,
         start_urls=start_urls,
--- hiik_helper/spiders/hiik_default_spider.py
+++ hiik_helper/spiders/hiik_default_spider.py
@@ -1,7 +1,16 @@
 import scrapy
+import json
 import re
+import datetime
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+from zoneinfo import ZoneInfo
 from scrapy.linkextractors import LinkExtractor
-from urllib import parse
+from scrapy.signals import spider_closed
+from scrapy.signalmanager import dispatcher
 
 
 class HIIKDefaultSpider(scrapy.Spider):
@@ -10,47 +19,89 @@ class HIIKDefaultSpider(scrapy.Spider):
     def __init__(
         self, start_urls: list[str], allowed_domains: list[str], *args, **kwargs
     ):
+        logger.info("Initializing spider")
         super(HIIKDefaultSpider, self).__init__(*args, **kwargs)
+        # Add spider close handler to save the found articles to a JSON file
+        dispatcher.connect(self.spider_closing, signal=spider_closed)
+
         self.start_urls: list[str] = start_urls
         self.allowed_domains: list[str] = allowed_domains
         self.article_link_domains: list[str] = [
             "karennews.org/category/article",
-            # "karennews.org/202",
+            "karennews.org/2024",
         ]
-        self.found_articles: list[str] = []
+
+        self.found_articles: dict[str, dict[str, str]] = {}
         self.content_class_article = "entry-content entry clearfix"
         self.content_class_list = "entry-content"
+
+        self.visited_urls_this_scrape = set()
+
+        logger.info("Loading visited URLs from JSON")
+        # Load JSON file with URLs that we have already visited
+        with open("visited_urls.json", "r") as f:
+            self.visited_json_urls = json.load(f)
+
+        logger.info("Spider initialized")
 
     def parse(self, response):
         article_links: set = set()
 
-        # Get content of the article on the page
-        article_content = self.parse_article(response)
+        url = response.url
 
-        if self.current_page_is_article(response):
+        if (
+            self.current_page_is_article(response)
+            and url not in self.visited_json_urls
+            and url not in self.visited_urls_this_scrape
+        ):
+            # Get content of the article on the page
+            article_content = self.parse_article(response)
+
             # Add the article content to the list of found articles
 
             for supposed_article in article_content:
+                # Get article:modified_time from meta property
+                article_modified_time = response.xpath(
+                    "//meta[@property='article:published_time']/@content"
+                ).get()
+
+                # Get the headline of the article
+                headline = response.xpath("//h1/text()").get()
+
+                # Get subheadline of the article
+                subheadline = response.xpath("//h2/text()").get()
+
                 # Get all paragraphs of the article
-                paragraphs = response.xpath("//p")
-                article_text = "\n".join(paragraphs.getall())
+                paragraphs = response.xpath("//p/text()").getall()
+                paragraph_text = "\n\n".join(paragraphs)
 
                 # Add the article to the list of found articles
-                self.found_articles.append(article_text)
+                article_dict = {
+                    "url": url,
+                    "accessing-date": str(datetime.datetime.now(datetime.UTC)),
+                    "last-modification": article_modified_time,
+                    "headline": headline,
+                    "subheadline": subheadline,
+                    "paragraphs": paragraph_text,
+                }
+                self.found_articles[url] = article_dict
+
+                self.visited_urls_this_scrape.add(url)
 
         elif self.current_page_is_list(response):
-            for supposed_article in article_content:
+            # Get content of the article on the page
+            article_list = self.parse_article_list(response)
+
+            for supposed_article in article_list:
                 # Get the more link of the article
                 more_link_url = self.get_url_in_article(supposed_article)
                 if self.link_is_article(more_link_url):
                     article_links.add(more_link_url)
 
-
         # Extract all links from the page to find new article pages
         extracted_links = LinkExtractor(
             allow_domains=self.allowed_domains
         ).extract_links(response)
-
         # Filter out the article links
         article_links = article_links.union(
             set([link for link in extracted_links if self.link_is_article(link.url)])
@@ -72,22 +123,21 @@ class HIIKDefaultSpider(scrapy.Spider):
             return True
         return False
 
+    def parse_article(self, response):
+        # Parse the article content
+        # Mocked implementation
+        return response.xpath(f'//div[@class="{self.content_class_article}"]').getall()
+
+    def parse_article_list(self, response):
+        # Parse the article content
+        # Mocked implementation
+        return response.xpath(f'//div[@class="{self.content_class_list}"]').getall()
+
     def link_is_article(self, url):
         # Check if the link is an article
         for article_link_domain in self.article_link_domains:
             if article_link_domain in url:
                 return True
-
-    def save_found_articles_to_json(self):
-        # Save the found articles to a JSON file
-        # Mocked implementation
-        with open("found_articles.json", "w") as f:
-            f.write(self.found_articles)
-
-    def parse_article(self, response):
-        # Parse the article content
-        # Mocked implementation
-        return response.xpath(f'//div[@class="{self.content_class}"]').getall()
 
     def article_contains_more_link(self, article):
         # Check if the article contains a more link
@@ -98,3 +148,38 @@ class HIIKDefaultSpider(scrapy.Spider):
         # Get the more link in the article
         url = re.findall(r'href="([^"]*)"', article)[0]
         return url
+
+    def clean_text_from_html(self, text):
+        # Clean the text from HTML tags
+        # Mocked implementation
+        return re.sub(r"<[^>]*>", "", text)
+
+    def save_found_articles_to_json(self):
+        json_file_name = "found_articles.json"
+
+        # Save the found articles to a JSON file
+        with open(json_file_name, "r") as f:
+            data = json.load(f)
+
+        data.update(self.found_articles)
+
+        with open(json_file_name, "w") as f:
+            json.dump(data, f, indent=4)
+
+    def save_visited_urls_to_json(self):
+        json_file_name = "visited_urls.json"
+
+        self.visited_json_urls.extend(self.visited_urls_this_scrape)
+
+        # Save the visited URLs to a JSON file
+        with open(json_file_name, "w") as f:
+            json.dump(list(self.visited_json_urls), f, indent=4)
+
+    def spider_closing(self, spider):
+        logger.info("Spider closing")
+
+        logger.info(f"Saving visited URLs to JSON")
+        self.save_visited_urls_to_json()
+
+        logger.info(f"Saving found articles to JSON")
+        self.save_found_articles_to_json()
